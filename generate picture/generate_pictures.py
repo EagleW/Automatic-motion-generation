b1f7599090d67c2701d4d70aecf965f538870dfa
unknown <dalewanghz@gmail.com>
2017-08-09 07:59:04 -0400

    fix

--- generate picture/generate_pictures.py
+++ generate picture/generate_pictures.py
@@ -1,28 +1,39 @@
 import sys, os, re
 from PIL import Image
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-allList_back = os.listdir(dir_path)
-all_pictures = []
-# Setting the regular expression to only look for .jpg extenstion
-jpgRE = re.compile('^\w+.jpg$', re.I)
-# Removing any files that do not have an .fbx extenstion
-for fname in allList_back:
-    bk = jpgRE.search(fname)
-    if bk:
-        all_pictures.append(fname)
-images = map(Image.open, all_pictures)
-widths, heights = zip(*(i.size for i in images))
+while(1):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    x = input()
+    dir_path += x
+    print (dir_path)
+    allList_back = os.listdir(dir_path)
+    all_pictures = []
+    # Setting the regular expression to only look for .jpg extenstion
+    jpgRE = re.compile('^\w+.jpg$', re.I)
+    # Removing any files that do not have an .fbx extenstion
+    for fname in allList_back:
+        bk = jpgRE.search(fname)
+        if bk:
+            all_pictures.append(fname)
+    images = map(Image.open, all_pictures)
+    widths, heights = zip(*(i.size for i in images))
 
-total_width = sum(widths)
-max_height = max(heights)
+    total_width = sum(widths)
+    max_height = max(heights)
 
-new_im = Image.new('RGB', (total_width, max_height))
+    new_im = Image.new('RGB', (total_width, max_height))
 
-x_offset = 0
-for im in images:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
-  print(im.format, im.size, im.mode)
+    x_offset = 0
+    y_offset = 0
+    n = 0
+    for im in images:
+        new_im.paste(im, (x_offset,y_offset))
+        n += 1
+        if n % 4 == 0:
+            x_offset = 0
+            y_offset += im.size[1]
+        else:
+            x_offset += im.size[0]
+      print(im.format, im.size, im.mode)
 
-new_im.save('toal.jpg')
+    new_im.save('toal.jpg')
